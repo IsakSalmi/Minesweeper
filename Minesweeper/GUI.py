c15cf6868931d99ca4332dc1c58e8f78f40253c9
isakSalmi <hansisak11@gmail.com>
2023-01-17 13:02:04 +0100

    got som simple gui funktion

--- Minesweeper/GUI.py
+++ Minesweeper/GUI.py
@@ -1,12 +1,18 @@
 import pygame as p
 import Minesweeper.Config as Config
+import Minesweeper.MinesweeperEngien as Engien
 
 
 BOARD_HIGHT = Config.BOARD_HIGHT
 BOARD_WIDTH = Config.BOARD_WIDTH
 MAX_FPS = Config.MAX_FPS
+DIMENTION = Config.DIMENTIONS
+SQ_SIZE = Config.SQ_SIDE 
 
 def StartGame():
+    
+    gs = Engien.MinesweeperEngien()
+    
     Screen = p.display.set_mode((BOARD_WIDTH,BOARD_HIGHT))
     Clock = p.time.Clock()
     Screen.fill(p.Color('Black'))
@@ -15,5 +21,32 @@ def StartGame():
         for e in p.event.get():
             if e.type == p.QUIT:
                 start = False
+            elif e.type == p.MOUSEBUTTONDOWN:
+                if e.button == 1:
+                    location = p.mouse.get_pos()
+                    col = int(location[0] // SQ_SIZE)
+                    row = int (location[1] // SQ_SIZE)
+                    gs.SelectSQ(row,col)
+                elif e.button == 3:
+                    location = p.mouse.get_pos()
+                    col = int(location[0] // SQ_SIZE)
+                    row = int (location[1] // SQ_SIZE)
+                    gs.flagSQ(row,col)
+                
+                
+        drawGameState(Screen,gs)
         Clock.tick(MAX_FPS)
         p.display.flip()
+        
+        
+        
+def drawGameState(screen,gs):
+    Board = gs.GetBoard()
+    for r in range(DIMENTION):
+        for c in range(DIMENTION):
+            if Board[r][c] == '-':
+                p.draw.rect(screen, p.Color('Gray'), p.Rect(SQ_SIZE * c, SQ_SIZE * r, SQ_SIZE+1, SQ_SIZE+1))
+            elif Board[r][c] == 'F':
+                p.draw.rect(screen, p.Color('Red'), p.Rect(SQ_SIZE * c, SQ_SIZE * r, SQ_SIZE+1, SQ_SIZE+1))
+            else:
+                p.draw.rect(screen, p.Color('Black'), p.Rect(SQ_SIZE * c, SQ_SIZE * r, SQ_SIZE+1, SQ_SIZE+1))
--- Minesweeper/MinesweeperEngien.py
+++ Minesweeper/MinesweeperEngien.py
@@ -1,7 +1,37 @@
-
+import Minesweeper.Config as Config
 
 class MinesweeperEngien:
     def __init__(self):
-        self.Board = []
+        self.GameBoard = [['-','-','-','-','-','-','-','-','-'],
+                        ['-','-','-','-','-','-','-','-','-'],
+                        ['-','-','-','-','-','-','-','-','-'],
+                        ['-','-','-','-','-','-','-','-','-'],
+                        ['-','-','-','-','-','-','-','-','-'],
+                        ['-','-','-','-','-','-','-','-','-'],
+                        ['-','-','-','-','-','-','-','-','-'],
+                        ['-','-','-','-','-','-','-','-','-'],
+                        ['-','-','-','-','-','-','-','-','-']]
+        self.DisplayBoard = self.GameBoard
+        self.Minse = Config.MINNES
+        self.GameStatus = True
+    
+    def GetBoard(self):
+        return self.DisplayBoard
+    
+    def SelectSQ(self, r, c):
+        if self.GameBoard == '*':
+            self.GameBoard = False
+            self.DisplayBoard[r][c] = '*'
+        else:
+            self.DisplayBoard[r][c] = ' '
+
+        
+    def flagSQ(self, r, c):
+        self.DisplayBoard[r][c] = 'F'
+    
+    
+    
+    
+        
 
 
