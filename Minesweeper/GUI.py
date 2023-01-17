6a5a40e687e41ced50ecfa6cfb7c366ade403272
isakSalmi <hansisak11@gmail.com>
2023-01-17 20:57:03 +0100

    hej

--- Minesweeper/GUI.py
+++ Minesweeper/GUI.py
@@ -6,32 +6,42 @@ import Minesweeper.MinesweeperEngien as Engien
 BOARD_HIGHT = Config.BOARD_HIGHT
 BOARD_WIDTH = Config.BOARD_WIDTH
 MAX_FPS = Config.MAX_FPS
-DIMENTION = Config.DIMENTIONS
+DIMENTION = Config.DEMANTION
 SQ_SIZE = Config.SQ_SIDE 
+
+p.init()
+
 
 def StartGame():
     
     gs = Engien.MinesweeperEngien()
+    FirstClick = False
     
     Screen = p.display.set_mode((BOARD_WIDTH,BOARD_HIGHT))
     Clock = p.time.Clock()
     Screen.fill(p.Color('Black'))
     start = True
     while start:
+        
         for e in p.event.get():
             if e.type == p.QUIT:
                 start = False
             elif e.type == p.MOUSEBUTTONDOWN:
-                if e.button == 1:
-                    location = p.mouse.get_pos()
-                    col = int(location[0] // SQ_SIZE)
-                    row = int (location[1] // SQ_SIZE)
-                    gs.SelectSQ(row,col)
-                elif e.button == 3:
-                    location = p.mouse.get_pos()
-                    col = int(location[0] // SQ_SIZE)
-                    row = int (location[1] // SQ_SIZE)
-                    gs.flagSQ(row,col)
+                if gs.GameStatus:
+                    if e.button == 1:
+                        location = p.mouse.get_pos()
+                        col = int(location[0] // SQ_SIZE)
+                        row = int (location[1] // SQ_SIZE)
+                        if FirstClick == False: 
+                            gs.createBoard(row,col)
+                            FirstClick = True
+                        gs.PrintGameboard()
+                        gs.SelectSQ(row,col)
+                    elif e.button == 3:
+                        location = p.mouse.get_pos()
+                        col = int(location[0] // SQ_SIZE)
+                        row = int (location[1] // SQ_SIZE)
+                        gs.flagSQ(row,col)
                 
                 
         drawGameState(Screen,gs)
@@ -42,11 +52,17 @@ def StartGame():
         
 def drawGameState(screen,gs):
     Board = gs.GetBoard()
+    font = p.font.SysFont('Arial', 30)
     for r in range(DIMENTION):
         for c in range(DIMENTION):
             if Board[r][c] == '-':
                 p.draw.rect(screen, p.Color('Gray'), p.Rect(SQ_SIZE * c, SQ_SIZE * r, SQ_SIZE+1, SQ_SIZE+1))
             elif Board[r][c] == 'F':
                 p.draw.rect(screen, p.Color('Red'), p.Rect(SQ_SIZE * c, SQ_SIZE * r, SQ_SIZE+1, SQ_SIZE+1))
-            else:
+            elif Board[r][c] == '0':
                 p.draw.rect(screen, p.Color('Black'), p.Rect(SQ_SIZE * c, SQ_SIZE * r, SQ_SIZE+1, SQ_SIZE+1))
+            else:
+                textObject = font.render(Board[r][c], 0, p.Color('white'))
+                textLocation = p.Rect((SQ_SIZE * c), (SQ_SIZE * r), (SQ_SIZE + SQ_SIZE * c), (SQ_SIZE + SQ_SIZE * r))
+                p.draw.rect(screen,p.Color("Black"),p.Rect(SQ_SIZE * c, SQ_SIZE * r, SQ_SIZE+1, SQ_SIZE+1))
+                screen.blit(textObject, textLocation)
--- Minesweeper/MinesweeperEngien.py
+++ Minesweeper/MinesweeperEngien.py
@@ -1,4 +1,5 @@
 import Minesweeper.Config as Config
+import random
 
 class MinesweeperEngien:
     def __init__(self):
@@ -11,21 +12,83 @@ class MinesweeperEngien:
                         ['-','-','-','-','-','-','-','-','-'],
                         ['-','-','-','-','-','-','-','-','-'],
                         ['-','-','-','-','-','-','-','-','-']]
-        self.DisplayBoard = self.GameBoard
-        self.Minse = Config.MINNES
+
+
+        self.DisplayBoard = [['-','-','-','-','-','-','-','-','-'],
+                            ['-','-','-','-','-','-','-','-','-'],
+                            ['-','-','-','-','-','-','-','-','-'],
+                            ['-','-','-','-','-','-','-','-','-'],
+                            ['-','-','-','-','-','-','-','-','-'],
+                            ['-','-','-','-','-','-','-','-','-'],
+                            ['-','-','-','-','-','-','-','-','-'],
+                            ['-','-','-','-','-','-','-','-','-'],
+                            ['-','-','-','-','-','-','-','-','-']]
+        self.Minse = Config.MINES
         self.GameStatus = True
     
     def GetBoard(self):
         return self.DisplayBoard
     
-    def SelectSQ(self, r, c):
-        if self.GameBoard == '*':
-            self.GameBoard = False
-            self.DisplayBoard[r][c] = '*'
+    def SelectSQ(self, sr, sc):
+        if self.GameBoard[sr][sc] == '-':
+            self.DisplayBoard[sr][sc] = '0'
+        elif self.GameBoard[sr][sc] != '*':
+            self.DisplayBoard[sr][sc] = self.GameBoard[sr][sc]
         else:
-            self.DisplayBoard[r][c] = ' '
-
+            self.GameBoard[sr][sc] == '*'
+            self.GameStatus = False
         
+        if self.GameStatus:
+            directions = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+            AvSQ = []
+            AvSQ.append((sr,sc))
+            while len(AvSQ) != 0:
+                r = AvSQ[0][0]
+                c = AvSQ[0][1]
+                print(r,c)
+                for d in directions:
+                    endRow = r + d[0]
+                    endCol = c + d[1]
+                    if endRow >= 0 and endRow < Config.DEMANTION and endCol >= 0 and endCol < Config.DEMANTION:
+                        if self.GameBoard[endRow][endCol] == '-':
+                            AvSQ.append((endRow,endCol))
+                            self.GameBoard[endRow][endCol] = '0'
+                            self.DisplayBoard[endRow][endCol] = '0'
+                        elif self.GameBoard[endRow][endCol] != '*':
+                            self.DisplayBoard[endRow][endCol] = self.GameBoard[endRow][endCol]
+                AvSQ.pop(0)
+        
+                
+            
+
+    def createBoard(self,sr,sc):
+        directions = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+        Mines = 0
+        while Mines < 10:
+            r = random.randint(0,Config.DEMANTION-1)
+            c = random.randint(0,Config.DEMANTION-1)
+            if  (r != sr) and (c != sc) and self.GameBoard[r][c] != '*':
+                self.GameBoard[r][c] = '*'
+                for d in directions:
+                    endRow = r + d[0]
+                    endCol = c + d[1]
+                    if endRow >= 0 and endRow < Config.DEMANTION and endCol >= 0 and endCol < Config.DEMANTION:
+                        if self.GameBoard[endRow][endCol] == '-':
+                            self.GameBoard[endRow][endCol] = '1'
+                        elif self.GameBoard[endRow][endCol] == '*':
+                            pass
+                        else:
+                            temp = self.GameBoard[endRow][endCol]
+                            temp = int(temp)
+                            temp += 1
+                            temp = str(temp)
+                            self.GameBoard[endRow][endCol] = temp
+                Mines += 1
+
+    def PrintGameboard(self):
+        for r in range(Config.DEMANTION):
+            print(self.GameBoard[r])
+
     def flagSQ(self, r, c):
         self.DisplayBoard[r][c] = 'F'
     
